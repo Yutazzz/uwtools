c556e2c60feab9727ced0acfc82e791b88238c73
Yutazzz <94127970+Yutazzz@users.noreply.github.com>
2022-04-12 02:42:13 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
       document.getElementById(clickedItem.dataset.id).classList.add('active');
     });
     homeclick.addEventListener('click', () => {
-      window.location.href = "index.html"
+      window.location.href = "../../index.html"
     });
   });
 }
@@ -31,3 +31,53 @@
 
 
 
+// スライドショー
+{
+  const images = [
+    'img/a.jpg',
+    'img/b.jpg',
+    'img/c.jpg',
+  ];
+  let currentIndex = 0;
+
+  const mainImage = document.getElementById('freedomeuwt-1');
+  mainImage.src = images[currentIndex];
+
+  images.forEach((image, index) => {
+    const img = document.createElement('img');
+    img.src = image;
+
+    const li = document.createElement('li');
+    if (index === currentIndex) {
+      li.classList.add('current');
+    }
+    li.addEventListener('click', () => {
+      mainImage.src = image;
+      const thumbnails = document.querySelectorAll('.thumbnails > li');
+      thumbnails[currentIndex].classList.remove('current');
+      currentIndex = index;
+      thumbnails[currentIndex].classList.add('current');
+    });
+
+    li.appendChild(img);
+    document.querySelector('.thumbnails').appendChild(li);
+  });
+
+  const next = document.getElementById('next');
+  next.addEventListener('click', () => {
+    let target = currentIndex + 1;
+    if (target === images.length) {
+      target = 0;
+    }
+    document.querySelectorAll('.thumbnails > li')[target].click();
+  });
+
+  const prev = document.getElementById('prev');
+  prev.addEventListener('click', () => {
+    let target = currentIndex - 1;
+    if (target < 0) {
+      target = images.length - 1;
+    }
+    document.querySelectorAll('.thumbnails > li')[target].click();
+  });
+}
